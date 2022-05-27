230b7d573c4ef1306e133834c8b9002a4dbe74ee
henriquewallace <henrique.wallace@hotmail.com>
2022-05-27 17:29:34 -0300

    RP - Create random-password component and install tailwind style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HW-random-password';
+  title = 'RP-random-password';
 }
---
+++ src/app/random-password/random-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-password',
+  templateUrl: './random-password.component.html',
+  styleUrls: ['./random-password.component.css']
+})
+export class RandomPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
