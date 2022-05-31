3d27afc3b5a4f96deba83794444fb26fe29a1ce6
henriquewallace <henrique.wallace@hotmail.com>
2022-05-30 22:41:30 -0300

    RP - Create and style the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RandomPasswordComponent } from './random-password/random-password.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RandomPasswordComponent
+    RandomPasswordComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
